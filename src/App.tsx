327de12cfb0263f56f24b38004026d1e9a406ac7
Damilola Bada <damilola.bada@apomap.de>
2024-03-14 07:17:27 +0100

    initial setup with husky, shadcn ui, engine locking, prettier and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-500">Vite + Shadcn Starter Template</h1>
+      <Button>Get started</Button>
+    </div>
+  );
+}
+
+export default App;
