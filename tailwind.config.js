4d54b9bf2366365a8a66d2f98e2944fa1f744ca4
Damilola Bada <damilola.bada@apomap.de>
2024-03-14 07:23:19 +0100

    setup storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}'
+    './src/**/*.{ts,tsx}',
+    './src/stories/**/*.{ts,tsx}',
+    './src/**/*.stories.{ts,tsx}'
   ],
   prefix: '',
   theme: {
