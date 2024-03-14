71a7877c9b03a2841673a1c4583e58db4b3da601
Damilola Bada <damilola.bada@apomap.de>
2024-03-14 07:52:30 +0100

    setup jest and react testing library for unit testing

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  preset: "ts-jest",
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/e2e/", // Exclude e2e directory
+  ],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^.+\\.svg$": "jest-svg-transformer",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+};
+
+export default config;
---
+++ src/app.test.tsx
@@ -0,0 +1,42 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("Homepage", () => {
+  it("renders a heading", () => {
+    render(<App />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders the correct text", () => {
+    render(<App />);
+
+    expect(
+      screen.getByText("Vite + Shadcn Starter Template"),
+    ).toBeInTheDocument();
+  });
+
+  it("renders a button on the homepage", () => {
+    render(<App />);
+
+    const button = screen.getByRole("button", {
+      name: "Get started",
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("renders a link to my website", () => {
+    render(<App />);
+
+    const link = screen.getByRole("link", {
+      name: "Visit my website",
+    });
+
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "https://www.damilolabada.com");
+  });
+});
